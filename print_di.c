281cb18719fcee2e4f070e93bf60b2b2c71016b3
macrespo <maxime.crespopro@gmail.com>
2019-11-13 19:07:38 +0100

    0, width flags supports for %d and i

--- print_di.c
+++ print_di.c
@@ -6,15 +6,15 @@
 /*   By: macrespo <macrespo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/13 18:37:56 by macrespo          #+#    #+#             */
-/*   Updated: 2019/11/13 18:55:00 by macrespo         ###   ########.fr       */
+/*   Updated: 2019/11/13 19:06:23 by macrespo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int	len_num(int n)
+static int		len_num(int n)
 {
-	int	len;
+	int		len;
 
 	len = 1;
 	if (n < 0)
@@ -33,13 +33,37 @@ static int	len_num(int n)
 	return (len);
 }
 
-int		print_di(va_list arg, t_flags flags)
+static int		print_width(t_flags flags, int size)
+{
+	char	zero;
+	int		printed;
+
+	zero = ' ';
+	if (flags.zero == 1 && flags.dash == 0)
+		zero = '0';
+	while (size++ < flags.width)
+	{
+		write(1, &zero, 1);
+		printed++;
+	}
+	return (printed);
+}
+
+int				print_di(va_list arg, t_flags flags)
 {
 	int		len;
 	int		nb;
+	int		printed;
 
+	printed = 0;
 	nb = va_arg(arg, int);
 	len = len_num(nb);
+	if (flags.width > 0 && flags.dash == 0)
+		printed += print_width(flags, len);
 	ft_putnbr_fd(nb, 1);
+	if (flags.width > 0 && flags.dash == 1)
+		printed += print_width(flags, len);
+	if (flags.width > 0)
+		return (printed);
 	return (len);
 }
